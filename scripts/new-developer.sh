8ba03ea6c8b17f063dea66f3e6864f3c05dcf028
Léo França <49125461+leoarkiteto@users.noreply.github.com>
2024-02-28 11:31:10 +0000

    feat: New Version of Engineering Blog (#16)

--- scripts/new-developer.sh
+++ scripts/new-developer.sh
@@ -1,6 +1,5 @@
 echo "Let's get started installing dependencies..."
 
-
 echo "Do you want to enable auto-signing your commits?"
 echo "Note: Will use key id_ed25519"
 echo "Note: Remember to upload your key as a signing key"
@@ -15,36 +14,6 @@ select yn in "Yes" "No"; do
         No ) break;;
     esac
 done
-
-
-# https://github.com/rbenv/rbenv
-echo "Do you want to install RBEnv"
-select yn in "Yes" "No"; do
-    case $yn in
-        Yes ) brew install rbenv ruby-build; break;;
-        No ) break;;
-    esac
-done
-
-
-echo "Do you want to switch to RBEnv 3.1.3 locally?"
-select yn in "Yes" "No"; do
-    case $yn in
-        Yes ) rbenv install 3.1.3; rbenv local 3.1.3; break;;
-        No ) break;;
-    esac
-done
-
-
-# https://jekyllrb.com/docs/installation/macos
-echo "Do you want to install Jekyll for GitHub Pages?"
-select yn in "Yes" "No"; do
-    case $yn in
-        Yes ) gem install jekyll; break;;
-        No ) break;;
-    esac
-done
-
 
 # https://commitizen-tools.github.io/commitizen/
 echo "Do you want to install Commitizen?"
