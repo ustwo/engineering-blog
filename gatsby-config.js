8fd4933bb5b78a1a5ca37798982c06569922dd8c
phil-linnell <phil@ustwo.com>
2024-08-15 19:39:45 +0100

    Copy files to public

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,6 +21,7 @@ module.exports = {
       resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [
+          `gatsby-remark-copy-linked-files`,
           {
             resolve: `gatsby-remark-images`,
             options: {
