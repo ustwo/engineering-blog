f2bdd38123f3ffd5bd05066dbe7630cd5ef3b61c
phil-linnell <phil@ustwo.com>
2024-10-08 09:22:57 +0100

    Refactor article-cta.js

--- src/components/article-cta.js
+++ src/components/article-cta.js
@@ -1,16 +1,10 @@
 import React from "react";
 import * as styles from "./article-cta.module.css";
 
-const ArticleCTA = ({prefix}) => {
-  if(prefix){
-    prefix += " ";
-  }
-
-  return (
-    <section className={styles.cta}>
-        <p>{prefix}If you'd like to have a chat about working with ustwo or joining our team, head over to <a href="https://ustwo.com">ustwo.com</a></p>
-    </section>
-  );
-}
+const ArticleCTA = ({ prefix }) => (
+  <section className={styles.cta}>
+      <p>{prefix && `${prefix} `}If you'd like to have a chat about working with ustwo or joining our team, head over to <a href="https://ustwo.com">ustwo.com</a></p>
+  </section>
+);
 
 export default ArticleCTA;
