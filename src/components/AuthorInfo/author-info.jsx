bfe449abe24712f6707c3320f660ce59bdb72621
patriciagssilva <patricia.silva@ustwo.com>
2024-10-08 17:13:27 +0100

    add author info component

---
+++ src/components/AuthorInfo/author-info.jsx
@@ -0,0 +1,84 @@
+import React from "react";
+import { graphql, useStaticQuery } from "gatsby";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
+import * as styles from "./author-info.module.css";
+
+const AuthorInfo = ({ authorName }) => {
+  const { authors } = useStaticQuery(graphql`
+    query {
+      authors: allFile(
+        filter: {
+          sourceInstanceName: { eq: "authors" }
+          extension: { eq: "md" }
+        }
+      ) {
+        nodes {
+          childMarkdownRemark {
+            frontmatter {
+              name
+              role
+              shortIntro
+              avatar {
+                childImageSharp {
+                  gatsbyImageData
+                }
+              }
+              contactInfo {
+                platform
+                url
+              }
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  const activeAuthor = authors.nodes.filter(
+    (author) => author.childMarkdownRemark.frontmatter.name === authorName
+  );
+  const { name, role, avatar, shortIntro, contactInfo } =
+    activeAuthor[0].childMarkdownRemark.frontmatter;
+  const avatarImage = getImage(avatar?.childImageSharp?.gatsbyImageData);
+
+  return (
+    <section className={styles.authorProfileSection}>
+      <h3>Author:</h3>
+      <div className={styles.authorHeader}>
+        <GatsbyImage
+          className={styles.authorAvatar}
+          image={avatarImage}
+          alt={`${name} headshot`}
+        />
+        <div className={styles.authorDetails}>
+          <h4 className={styles.authorName}>{name}</h4>
+          <p className={styles.authorRole}>{role}</p>
+        </div>
+      </div>
+
+      {!!shortIntro && (
+        <div className={styles.quoteBox}>
+          <p className={styles.quoteText}>{shortIntro}</p>
+        </div>
+      )}
+
+      {!!contactInfo && (
+        <div className={styles.authorContactInfo}>
+          {contactInfo.map((info, index) => (
+            <a
+              key={`contact-info-${index}`}
+              href={info.url}
+              target="_blank"
+              rel="noopener noreferrer"
+              className={styles.contactLink}
+            >
+              {info.platform}
+            </a>
+          ))}
+        </div>
+      )}
+    </section>
+  );
+};
+
+export default AuthorInfo;
--- src/content/authors/patricia-silva/index.md
+++ src/content/authors/patricia-silva/index.md
@@ -3,4 +3,13 @@ name: Patricia Silva
 role: Senior Fullstack Developer
 contact: patricia.silva@ustwo.com
 avatar: ./avatar-patricia-silva.jpg
+shortIntro: My first priority is people, followed by technology. However, by combining the two, I can help positively transform people's lives through technology. My first priority is people, followed by technology. However, by combining the two, I can help positively transform people's lives through technology.
+
+contactInfo:
+  - platform: LinkedIn
+    url: https://www.linkedin.com/in/patricia-silva-5304b233/
+  - platform: Twitter
+    url: https://twitter.com/patricia_silva
+  - platform: GitHub
+    url: https://github.com/patricia-silva
 ---
