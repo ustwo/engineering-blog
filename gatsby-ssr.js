b10460bb8f72d1970eb1429a61170fb660cf0c91
phil-linnell <phil@ustwo.com>
2024-10-10 10:31:10 +0100

    Add Cabin Analytics

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,3 +1,12 @@
-export function onRenderBody({ setHtmlAttributes }) {
+import React from "react";
+
+export function onRenderBody({ setPostBodyComponents, setHtmlAttributes }) {
   setHtmlAttributes({ lang: "en" });
+  setPostBodyComponents([
+    <script
+      async
+      defer
+      src="https://scripts.withcabin.com/hello.js"
+    />,
+  ]);
 };
